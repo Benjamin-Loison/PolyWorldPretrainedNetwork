8d500ac80df3a43f07d13b7fe8528b1ab8ff1b73
Benjamin Loison <benjamin_loison@users.noreply.gitea.lemnoslife.com>
2023-10-24 02:30:49 +0200

    #7: Add TPU support

--- prediction.py
+++ prediction.py
@@ -70,7 +70,7 @@ def prediction(batch_size, images_directory, annotations_path):
     predictions = []
     for i_batch, sample_batched in enumerate(train_iterator):
 
-        rgb = sample_batched['image'].cpu().float()
+        rgb = sample_batched['image'].to(device).float()
         idx = sample_batched['image_idx']
 
         t0 = time.time()
